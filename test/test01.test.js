b03b0d90e4843285d53559938b08509e9a8c9d77
Wilberto Pacheco Batista <wilbertopachecob@gmail.com>
2023-09-17 12:09:42 -0500

    improve error tracing and display msgs

--- test/test01.test.js
+++ test/test01.test.js
@@ -1,11 +1,23 @@
-beforeEach(() => {
-  console.log("BeforeEach");
-});
+describe("test01", () => {
+  beforeEach(() => {
+    // console.log("BeforeEach");
+  });
 
-it("expected to succeed using not", () => {
-  expect(1 === 1).not.toBe(false);
-});
+  it("expected to succeed using not", () => {
+    expect(1 === 1).not.toBe(false);
+  });
 
-it("expected to succeed", () => {
-  expect(1 === 1).toBe(true);
+  it("expected to succeed", () => {
+    expect(1 === 1).toBe(true);
+  });
+
+  it("throws an error", () => {
+    expect(function myCustomError() {
+      throw new Error("test");
+    }).toThrow();
+  });
+
+  it("not throws an error", () => {
+    expect(() => {}).not.toThrow();
+  });
 });
--- test/test02.test.js
+++ test/test02.test.js
@@ -1,3 +1,5 @@
-it("expected to fail", () => {
-  expect(1 === 1).toBe(false);
+describe("test02", () => {
+  it("expected to fail", () => {
+    expect(1 === 1).toBe(false);
+  });
 });
